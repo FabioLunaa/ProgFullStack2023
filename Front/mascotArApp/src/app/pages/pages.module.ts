d98016ef31960b278768a17baae42bf6936f1707
CelesteJohanna92 <celestereo90@gmail.com>
2023-05-01 13:05:50 -0300

    cd

--- Front/mascotArApp/src/app/pages/pages.module.ts
+++ Front/mascotArApp/src/app/pages/pages.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistroUsuarioComponent } from './registro.usuario/registro.usuario.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegistroUsuarioComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule
   ]
