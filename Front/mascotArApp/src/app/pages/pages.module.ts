ca6b0c797dacb89a9d0aca060db43fafc8ee74ae
viktoryia40 <ozarenko2719@gmail.com>
2023-05-01 20:49:15 -0300

    Solucione un error

--- Front/mascotArApp/src/app/pages/pages.module.ts
+++ Front/mascotArApp/src/app/pages/pages.module.ts
@@ -1,28 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { RegistroUsuarioComponent } from './registro.usuario/registro.usuario.component';
-import { LoginComponent } from './login/login.component';
-=======
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from '../layout/navbar/navbar.component';
->>>>>>> 845a5c93af0658ca0c7c0d53c27f6c5c94375207
+import { RegistroUsuarioComponent } from './registro.usuario/registro.usuario.component';
+
 
 
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
-    RegistroUsuarioComponent,
-=======
     HomeComponent,
->>>>>>> 845a5c93af0658ca0c7c0d53c27f6c5c94375207
-    LoginComponent
+    LoginComponent,
+    RegistroUsuarioComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [HomeComponent, LoginComponent]
+  exports: [HomeComponent, LoginComponent, RegistroUsuarioComponent]
 })
 export class PagesModule { }
