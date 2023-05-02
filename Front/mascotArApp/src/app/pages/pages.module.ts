7cdc3872fc2bd620ad07f248c6c49712b1a7ca8a
German Villalba <germanvillalba2690@gmail.com>
2023-05-02 11:46:30 -0300

    avance en modulos/componentes

--- Front/mascotArApp/src/app/pages/pages.module.ts
+++ Front/mascotArApp/src/app/pages/pages.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroUsuarioComponent } from './registro.usuario/registro.usuario.component';
+import { MascotasPerdidasComponent } from './MascotasPerdidas/MascotasPerdidas.component';
+
 
 
 
@@ -11,11 +13,12 @@ import { RegistroUsuarioComponent } from './registro.usuario/registro.usuario.co
   declarations: [
     HomeComponent,
     LoginComponent,
-    RegistroUsuarioComponent
+    RegistroUsuarioComponent,
+    MascotasPerdidasComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [HomeComponent, LoginComponent, RegistroUsuarioComponent]
+  exports: [HomeComponent, LoginComponent, RegistroUsuarioComponent,MascotasPerdidasComponent]
 })
 export class PagesModule { }
