9a260a549a96ce8ce7c48d28230eab7c1e2e8117
German Villalba <germanvillalba2690@gmail.com>
2023-05-05 12:12:36 -0300

    borrado routeo de layout

--- Front/mascotArApp/src/app/app.module.ts
+++ Front/mascotArApp/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; 
-import { LayoutModule } from './layout/layout.module';
 import { PagesModule } from './pages/pages.module';
 
 
@@ -13,7 +12,6 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LayoutModule,
     PagesModule,
   ],
   providers: [],
