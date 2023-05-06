98551e43f052b1ea33f681f75bf7b28436554946
MartinCorral883 <cmc_883@hotmail.com>
2023-05-05 23:45:24 -0300

    Se agrega componente Carrito

--- Front/mascotArApp/src/app/Productos/Productos.module.ts
+++ Front/mascotArApp/src/app/Productos/Productos.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductosComponent } from './Productos.component';
 import { VerProductosComponent } from './VerProductos/VerProductos.component';
+import { CarritoComponent } from './Carrito/Carrito.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    VerProductosComponent
+    VerProductosComponent,
+    CarritoComponent
   ],
-  declarations: [ProductosComponent,VerProductosComponent]
+  declarations: [ProductosComponent,VerProductosComponent,CarritoComponent]
 })
 export class ProductosModule { }
