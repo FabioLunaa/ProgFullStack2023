a30749d74a998a324644b2b2d75c964c1591dc30
MartinCorral883 <cmc_883@hotmail.com>
2023-05-20 21:57:16 -0300

    Se inyecta servicio a Mascotas en Adopcion

--- Front/mascotArApp/src/app/pages/MascotasAdopcion/MascotasAdopcion.component.ts
+++ Front/mascotArApp/src/app/pages/MascotasAdopcion/MascotasAdopcion.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-MascotasAdopcion',
@@ -7,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MascotasAdopcionComponent implements OnInit {
 
-  constructor() { }
+  pets: any[] = [];
+  mascotaUrl: string | undefined;
+
+  constructor(private http: HttpClient ) { }
+
+  ListaDeMascotas(): Observable <any>{
+    return this.http.get(this.mascotaUrl + "pets")
+  }
+
+  addMascotas(mascotas: any): void {
+    this.pets.push(mascotas);
+  }
+  removeMascotas(mascotas: any): void {
+    const index = this.pets.indexOf(mascotas);
+    if (index > -1) {
+      this.pets.splice(index, 1);
+
+    }
+  }
+  getMascotas(): any[] {
+    return this.pets;
+  }
+  clearMascotas(): void {
+    this.pets = [];
+  }
 
   ngOnInit() {
   }
