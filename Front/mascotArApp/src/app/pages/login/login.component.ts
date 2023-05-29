fdffee0bcdb9699d07558a3b9085ce46fd6e176c
CelesteJohanna92 <celestereo90@gmail.com>
2023-05-29 00:39:40 -0300

    subo Login reactivo con validaciones

--- Front/mascotArApp/src/app/pages/login/login.component.ts
+++ Front/mascotArApp/src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm=this.formBuilder.group({
+    email: ['email@email.com', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
 
-  constructor() { }
+  })
+
+  constructor(private formBuilder:FormBuilder, private router:Router) { }
 
   ngOnInit(): void {
+   
+  }
+  login(){
+    if(this.loginForm.valid){
+      console.log("Llamar al servicio de login");
+      this.router.navigateByUrl('/inicio');
+      this.loginForm.reset();
+    }
+    else{
+      this.loginForm.markAllAsTouched();
+      alert("Error al ingresar los datos")
+    }
   }
 
+  
 }
 
