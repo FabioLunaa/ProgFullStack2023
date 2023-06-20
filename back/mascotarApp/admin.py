9357f9cc7751a952d1a0d1eefe2bee86f733f3da
Fabio <fabiomarioluna@gmail.com>
2023-06-20 14:50:39 -0300

    modif. en admin del back

--- back/mascotarApp/admin.py
+++ back/mascotarApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Producto
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion', 'precio')
+    
+admin.site.register(Producto, ProductoAdmin)
