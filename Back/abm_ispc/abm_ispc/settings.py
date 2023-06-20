ef46fa6ba3734e19b64c0c4dd879768d626e08c2
Fabio <fabiomarioluna@gmail.com>
2023-06-19 23:54:49 -0300

    subiendo nuevos cambios de front

--- Back/abm_ispc/abm_ispc/settings.py
+++ Back/abm_ispc/abm_ispc/settings.py
@@ -45,13 +45,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'abm_ispc.urls'
@@ -153,4 +154,3 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
 CORS_ORIGIN_WHITELIST = ['http://localhost:4200']
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
